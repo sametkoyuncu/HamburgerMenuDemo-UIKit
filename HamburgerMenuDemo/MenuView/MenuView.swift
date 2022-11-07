f864d372d991e02a77efeb52be8d5502f236b951
Samet Koyuncu <sametkoyuncu@live.com>
2022-11-07 23:06:32 +0300

    Initial Commit

---
+++ HamburgerMenuDemo/MenuView/MenuView.swift
@@ -0,0 +1,50 @@
+//
+//  MenuView.swift
+//  HamburgerMenuDemo
+//
+//  Created by Samet Koyuncu on 7.11.2022.
+//
+
+import UIKit
+
+protocol MenuViewDelegate: AnyObject {
+    func didCloseButtonTapped()
+}
+
+@IBDesignable
+class MenuView: UIView {
+    
+    weak var delegate: MenuViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    @IBAction func ad(_ sender: Any) {
+        print("aasdd")
+    }
+  
+    @IBAction func mBtn(_ sender: Any) {
+        print("m")
+    }
+}
+
+// delegate?.didCloseButtonTapped()
+
+private extension MenuView {
+    func commonInit() {
+        let bundle = Bundle(for: MenuView.self)
+        
+        if let viewToAdd = bundle.loadNibNamed("MenuView", owner: self, options: nil), let contentView = viewToAdd.first as? UIView {
+            addSubview(contentView)
+            contentView.frame = self.bounds
+            contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        }
+    }
+}
---
+++ HamburgerMenuDemo/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  HamburgerMenuDemo
+//
+//  Created by Samet Koyuncu on 7.11.2022.
+//
+
+import UIKit
+
+enum MenuState {
+    case open
+    case close
+}
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var hamburgerMenuView: MenuView!
+    
+    private let menuView = MenuView()
+    
+    
+    var menuState: MenuState = .open
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //menuView.delegate = self
+        
+        hamburgerMenuView.delegate = self
+    }
+
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("button tapped")
+        switch menuState {
+        case .open:
+            UIView.animate(withDuration: 0.5,
+                           delay: 0,
+                           usingSpringWithDamping: 0.8,
+                           initialSpringVelocity: 0,
+                           options: .curveEaseInOut) { [weak self] in
+                self?.hamburgerMenuView.frame.origin.x = -280
+            }
+            menuState = .close
+        case .close:
+            UIView.animate(withDuration: 0.5,
+                           delay: 0,
+                           usingSpringWithDamping: 0.8,
+                           initialSpringVelocity: 0,
+                           options: .curveEaseInOut) { [weak self] in
+                self?.hamburgerMenuView.frame.origin.x = -10
+            }
+            menuState = .open
+        }
+
+    }
+    
+}
+
+extension ViewController: MenuViewDelegate {
+    func didCloseButtonTapped() {
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       usingSpringWithDamping: 0.8,
+                       initialSpringVelocity: 0,
+                       options: .curveEaseInOut) { [weak self] in
+            self?.hamburgerMenuView.frame.origin.x = -280
+        }
+        menuState = .close
+    }
+}
